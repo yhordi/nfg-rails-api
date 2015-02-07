1c0205a2889f8c167709d6126222ae9e4fc2674b
Jordan Kamin <apprentice@DBC-infinity-too-4.local>
2015-02-07 12:29:46 -0800

    creates factories for tests

---
+++ spec/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  factory :user do
+    username { Faker::Internet.user_name(8) }
+    password 'password'
+  end
+  factory :post do
+    title { Faker::Lorem.sentence }
+    body { Faker::Lorem.paragraph }
+    author { Faker::Internet.user_name(8) }
+  end
+  factory :event do
+    summary { Faker::Lorem.sentence }
+    description { Faker::Lorem.paragraph }
+    time { Faker::Time.forward(23, :morning) }
+    readable_time { Faker::Date.forward(23) }
+  end
+  factory :video do
+    link { "//www.youtube.com/embed/_yOlihvcdLA" }
+  end
+end
