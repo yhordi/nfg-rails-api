1b8cb2230ee76c1b8b4de2c2c5b7f341fed8245a
Jordan Kamin <apprentice@DBC-infinity-too-4.local>
2015-02-07 12:13:20 -0800

    creates posts migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207200913) do
+ActiveRecord::Schema.define(version: 20150207201206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string "username",        null: false
