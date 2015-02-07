72f99b98ef04620470ab4f93d3c827691b6f5b9e
Jordan Kamin <apprentice@DBC-infinity-too-4.local>
2015-02-07 12:21:10 -0800

    creates videos migration. Migrations up to date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207201824) do
+ActiveRecord::Schema.define(version: 20150207201945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 20150207201824) do
   create_table "users", force: true do |t|
     t.string "username",        null: false
     t.string "password_digest", null: false
+  end
+
+  create_table "videos", force: true do |t|
+    t.string "link"
   end
 
 end
